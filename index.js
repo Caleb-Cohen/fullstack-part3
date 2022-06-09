c5f9c5659ac23fe0243076b463087ec97b405e6a
Caleb Cohen <calebcohen@hotmail.com>
2022-06-09 14:09:51 -0500

    3.1 update updated app.get

--- index.js
+++ index.js
@@ -21,6 +21,28 @@ let notes = [
     important: true
   }
 ]
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
 
 app.use(express.json())
 
@@ -67,12 +89,23 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
+// app.get('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   const note = notes.find(note => note.id === id)
 
-  if (note) {
-    response.json(note)
+//   if (note) {
+//     response.json(note)
+//   } else {
+//     response.status(404).end()
+//   }
+// })
+
+app.get('/api/persons', (request, response) => {
+  const id = Number(request.params.id)
+  const note = persons.find(persons => persons.id === id)
+
+  if (persons) {
+    response.json(persons)
   } else {
     response.status(404).end()
   }
