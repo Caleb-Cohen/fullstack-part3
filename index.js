6c18028e4e326ca85f8a238bff9fe0b989299e04
Caleb Cohen <calebcohen@hotmail.com>
2022-06-11 20:22:22 -0500

    3.5

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-01-10T19:20:14.298Z",
-    important: true
-  }
-]
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     date: "2022-01-10T17:30:31.098Z",
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only Javascript",
+//     date: "2022-01-10T18:39:34.091Z",
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     date: "2022-01-10T19:20:14.298Z",
+//     important: true
+//   }
+// ]
 let persons = [
   { 
     "id": 1,
@@ -44,49 +45,61 @@ let persons = [
   }
 ]
 
-app.use(express.json())
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
     : 0
   return maxId + 1
 }
 
-app.post('/api/notes', (request, response) => {
-  const body = request.body
+// app.post('/api/notes', (request, response) => {
+//   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+//   if (!body.content) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
+//   const note = {
+//     content: body.content,
+//     important: body.important || false,
+//     date: new Date(),
+//     id: generateId(),
+//   }
+
+//   notes = notes.concat(note)
+
+//   response.json(note)
+// })
+
+app.post('/api/persons', (req, res) => {
+  const body  = req.body
+
+  let entry = {
     id: generateId(),
+    name: body.name,
+    number: body.number
   }
 
-  notes = notes.concat(note)
+  person = persons.push(entry)
 
-  response.json(note)
+  res.json(entry)
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
+// app.get('/api/notes', (req, res) => {
+//   res.json(notes)
+// })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
-})
+// app.delete('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   notes = notes.filter(note => note.id !== id)
+//   response.status(204).end()
+// })
 
 app.delete('/api/persons/:id',(req,res) => {
   const id = Number(req.params.id)
